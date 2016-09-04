2adfd3e5864ef5160414c2c000090c7cd18aec10
dike.zhang <dike.zhang@gmail.com>
2016-09-04 11:29:27 -0400

    added bfs

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ bpm engine for python
 
 ## packages
 sqlalchemy
+
+http://stackoverflow.com/questions/713508/find-the-paths-between-two-given-nodes
+http://www.eecs.yorku.ca/course_archive/2006-07/W/2011/Notes/BFS_part1.pdf
--- src/flow/bfs.py
+++ src/flow/bfs.py
@@ -1,2 +1,49 @@
-'''Breadth-First-Search'''
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self,val):
+        self.queue.append(val)
+
+    def dequeue(self):
+        val = None
+        try:
+            val = self.queue[0]
+            if len(self.queue) == 1:
+                self.queue = []
+            else:
+                self.queue = self.queue[1:]    
+        except:
+            pass
+
+        return val
+
+    def isEmpty(self):
+        result = False
+        if len(self.queue) == 0:
+            result = True
+        return result
+    
+def BFS(graph,start,end,q):
+    '''Breadth-First-Search'''
+    temp_path = [start]
+
+    q.enqueue(temp_path)
+
+    while not q.isEmpty():
+        tmp_path = q.dequeue()
+        
+        last_node = tmp_path[len(tmp_path)-1]
+        
+        print tmp_path
+        if last_node == end:
+            print "VALID_PATH : ",tmp_path
+            
+        for link_node in graph[last_node]:
+            if link_node not in tmp_path:
+                #new_path = []
+                new_path = tmp_path + [link_node]
+                q.enqueue(new_path)
 
